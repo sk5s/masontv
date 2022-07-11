00579090c50708c4fe1d54ac7c806cb1d920056e
sk5s <samko5sam@tutanota.com>
2022-07-11 14:48:37 +0800

    update iframe link

--- src/pages/TvChannel.svelte
+++ src/pages/TvChannel.svelte
@@ -32,7 +32,7 @@
         <button on:click={() => sidebarOpen = !sidebarOpen} type="button" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Sidebar</button>
       </div>
       {#if channel.type == "yt"}
-          <iframe class="w-full h-full" width="560" height="315" src="https://www.youtube-nocookie.com/embed/{channel.videoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe class="w-full h-full" width="560" height="315" src="https://www.youtube-nocookie.com/embed/{channel.videoId}?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       {/if}
     </div>
   {/if}
